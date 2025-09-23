8c95561aeeabb535d63316ef8b8d00ad6d3e93ce
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-09-23 00:43:48 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+$title = "Anime Nutshell";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?> - Home</title>
+</head>
+<body>
+    <h1><?= $title ?></h1>
+    <p>Welcome! This is our starting page.</p>
+
+    <?php 
+
+    $anime = [
+        [
+            "title" => "Naruto",
+            "episodes" => "220",
+            "rating" => 8.2
+        ],
+        [
+            "title" => "One Piece",
+            ""
+
+        ]
+    ]
+
+    ?>
+
+
+    
+</body>
+</html>
